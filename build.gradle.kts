39d9eec93e974031fdc50c4210b2e24422d5bb2a
plusminusios <timofeikalimullin2014@gmail.com>
2026-05-24 23:49:01 +0500

    Delete build.gradle.kts

--- build.gradle.kts
+++
@@ -1,7 +0,0 @@
-plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.serialization) apply false
-    alias(libs.plugins.kotlin.compose) apply false
-    // FIX: secrets plugin removed (caused build failure with Gradle 9.x)
-}
