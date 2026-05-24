56b7120e5ce62c8d5970d10e40af18e06e5847a7
plusminusios <timofeikalimullin2014@gmail.com>
2026-05-24 23:44:01 +0500

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+    // FIX: secrets plugin removed (caused build failure with Gradle 9.x)
+}
